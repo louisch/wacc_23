1c9c07b8474edafb7650bcb535a126b5104417e4
Louis Chan <louisch@hacari.com>
2014-11-11 15:14:00 +0000

    Add AST abstract class

---
+++ src/wacc23/ast/AST.java
@@ -0,0 +1,13 @@
+package wacc23.ast;
+
+/**
+ * Represents a node of the abstract syntax tree of the program.
+ *
+ * May represent any individual part of a program, such as a statement, an
+ * expression, or the program as a whole.
+ * Used by the compiler to check for semantic errors, as well as a data
+ * structure to store the program, so that code may be generated from it.
+ */
+public abstract class AST extends antlr.BasicParserBaseVisitor<AST> {
+    public abstract void Check();
+}
