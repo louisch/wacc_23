a57b0d37f68b983e0684d2a16191ca2de1c17328
Louis Chan <louisch@hacari.com>
2014-11-12 20:23:55 +0000

    Add comments to ProgramAST

--- src/wacc23/ast/ProgramAST.java
+++ src/wacc23/ast/ProgramAST.java
@@ -6,15 +6,28 @@ import org.antlr.v4.runtime.misc.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents the abstract syntax tree of a WACC program
+ *
+ * Designed to be used to visit the ParseTree corresponding to the program rule
+ * of an ANTLR file.
+ */
 public class ProgramAST extends AST {
 
     private Map<String, AST> funcTable;
     private AST statement;
 
+    /**
+     * Constructs a ProgramAST with an empty function table.
+     */
     public ProgramAST() {
         this.funcTable = new HashMap<String, AST>();
     }
 
+    /**
+     * Constructs a ProgramAST with the given function table data structure.
+     * @param funcTable Table for containing function names and their ASTs
+     */
     public ProgramAST(Map<String, AST> funcTable) {
         this.funcTable = funcTable;
     }
