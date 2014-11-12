f44363fe6db228213c6ae1f5332451f16cf3471d
Louis Chan <louisch@hacari.com>
2014-11-12 19:52:38 +0000

    Create body field to contain field of function

--- src/wacc23/ast/FunctionAST.java
+++ src/wacc23/ast/FunctionAST.java
@@ -6,10 +6,13 @@ import org.antlr.v4.runtime.misc.NotNull;
 public class FunctionAST extends AST {
 
     String identifier;
+    AST body;
 
     @Override
     public AST visitFunc(@NotNull WaccParser.FuncContext ctx) {
         identifier = ctx.IDENT().getText();
+        body = new StatementAST();
+        body.visit(ctx.stat());
         return this;
     }
 
