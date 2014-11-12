7990aef2ccb2e4d705e3f181430226a57f06ec60
Louis Chan <louisch@hacari.com>
2014-11-12 18:56:15 +0000

    Add constructors for ProgramAST

--- src/wacc23/ast/ProgramAST.java
+++ src/wacc23/ast/ProgramAST.java
@@ -3,12 +3,21 @@ package wacc23.ast;
 import antlr.WaccParser;
 import org.antlr.v4.runtime.misc.NotNull;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProgramAST extends AST {
 
     private Map<String, AST> funcTable;
     private AST statement;
+
+    public ProgramAST() {
+        this.funcTable = new HashMap<String, AST>();
+    }
+
+    public ProgramAST(Map<String, AST> funcTable) {
+        this.funcTable = funcTable;
+    }
 
     @Override
     public AST visitProgram(@NotNull WaccParser.ProgramContext ctx) {
