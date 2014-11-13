aa270f66ea780f24a0739fbbdbf6072ba2554dc2
Louis Chan <louisch@hacari.com>
2014-11-13 15:43:14 +0000

    Add argument handling to Compiler

--- src/wacc23/Compiler.java
+++ src/wacc23/Compiler.java
@@ -2,6 +2,7 @@ package wacc23;
 
 import antlr.WaccParser;
 import antlr.WaccLexer;
+import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -15,9 +16,16 @@ import java.io.IOException;
  *
  * The main method of this class can be used to compile the program from
  * standard input.
- * TODO: Allow program to be specified as a filename.
  */
 public class Compiler {
+
+    public static String helpMessage() {
+        return "The compiler takes 0 or 1 arguments. If an argument is " +
+               "given, it must be a filename, which should be a WACC program " +
+               "file.\n" +
+               "If no arguments are given, then stdin is taken as input, and " +
+               "parsed as a WACC program.\n";
+    }
 
     public static WaccParser createParseTree(ANTLRInputStream inputStream) {
         WaccLexer lexer = new WaccLexer(inputStream);
@@ -26,10 +34,26 @@ public class Compiler {
     }
 
     /**
+     * <p>Runs the compiler, either using stdin as input, or a given file.</p>
+     *
+     * <p>If an argument is given, then it is taken to be the filename of a wacc
+     * file to be processed.</p>
      * @param args - Arguments passed from the command line
      */
     public static void main(String[] args) throws IOException {
-        WaccParser parser = createParseTree(new ANTLRInputStream(System.in));
+        // Get input either from System.in, or the first argument to main.
+        ANTLRInputStream input;
+        if (args.length == 1) {
+            input = new ANTLRInputStream(System.in);
+        } else if (args.length == 2) {
+            input = new ANTLRFileStream(args[1]);
+        } else {
+            System.out.print(helpMessage());
+            throw new IllegalArgumentException("Wrong number of arguments " +
+                                               "passed to main.");
+        }
+
+        WaccParser parser = createParseTree(input);
         ParseTree parseTree = parser.program();
 
         ProgramVisitor programVisitor = new ProgramVisitor();
