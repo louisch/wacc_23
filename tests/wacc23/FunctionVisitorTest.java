fd0be31689006a1c3dacb66ce69988da48b8ed3e
Louis Chan <louisch@hacari.com>
2014-11-13 02:23:10 +0000

    Write test for FunctionVisitor

---
+++ tests/wacc23/FunctionVisitorTest.java
@@ -0,0 +1,26 @@
+package wacc23;
+
+import antlr.WaccParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.junit.Test;
+import wacc23.ast.FunctionAST;
+import wacc23.visitor.FunctionVisitor;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FunctionVisitorTest {
+
+    private FunctionVisitor functionVisitor = new FunctionVisitor();
+
+    @Test
+    public void TestFunctionASTHasCorrectIdentifier() {
+        String funcIdent = "BillyBobby3";
+        String inputFunction = "int " + funcIdent + " () is skip; skip end";
+        WaccParser parser = Compiler.createParseTree(
+                new ANTLRInputStream(inputFunction));
+        FunctionAST functionAST = functionVisitor.visit(parser.func());
+
+        assertThat(functionAST.getIdentifier(), is(funcIdent));
+    }
+}
