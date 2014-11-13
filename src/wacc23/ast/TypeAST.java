3ab782e0bcda02776126bab9df37fe6418dcc72f
Louis Chan <louisch@hacari.com>
2014-11-13 02:23:33 +0000

    Add stub files for Types

---
+++ src/wacc23/ast/TypeAST.java
@@ -0,0 +1,8 @@
+package wacc23.ast;
+
+public class TypeAST extends AST {
+    @Override
+    public void check() {
+
+    }
+}
---
+++ src/wacc23/visitor/TypeVisitor.java
@@ -0,0 +1,7 @@
+package wacc23.visitor;
+
+import antlr.WaccParserBaseVisitor;
+import wacc23.ast.TypeAST;
+
+public class TypeVisitor extends WaccParserBaseVisitor<TypeAST> {
+}
