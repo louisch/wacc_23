f30b464c37f700684f33a1731d9ffd61979ab568
Alan Han <alan.y.han@gmail.com>
2014-11-14 23:12:37 +0000

    Added static method getIntType to BaseTypeAST

--- src/wacc23/ast/BaseTypeAST.java
+++ src/wacc23/ast/BaseTypeAST.java
@@ -1,14 +1,15 @@
 package wacc23.ast;
 
-/**
- * Created by Adam on 14/11/2014.
- */
 public class BaseTypeAST extends TypeAST {
 
     public BaseTypeAST(String type) {
         super(type);
     }
 
+    public static BaseTypeAST getIntType() {
+        return new BaseTypeAST("int");
+    }
 
+    // TODO: Write other get_Type methods
 }
 
