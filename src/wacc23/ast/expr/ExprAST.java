79c5ea43cf93cef2ce02a7e0b60c7eab2125db48
Alan Han <alan.y.han@gmail.com>
2014-11-15 15:55:51 +0000

    Removed comment conversation in ExprAST since it's resolved

--- src/wacc23/ast/expr/ExprAST.java
+++ src/wacc23/ast/expr/ExprAST.java
@@ -4,7 +4,5 @@ import wacc23.ast.AST;
 import wacc23.type.Type;
 
 public abstract class ExprAST extends AST {
-    // should this extend TypeAST instead?
-    // Anton: no, expressions are not types.
     public abstract Type getType();
 }
