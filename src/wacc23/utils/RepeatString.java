e7c7970436718264ad7b688cbaef4eb8a3b8b4dd
Alan Han <alan.y.han@gmail.com>
2014-11-15 01:29:52 +0000

    Added documentation to RepeatString

--- src/wacc23/utils/RepeatString.java
+++ src/wacc23/utils/RepeatString.java
@@ -1,6 +1,14 @@
 package wacc23.utils;
 
 public class RepeatString {
+    /**
+     * This repeats a string n number of times and returns it.
+     * Because I didn't want to use:
+     * new String(new char[n]).replace("\0", str);
+     *
+     * @param str String to be repeated
+     * @param n Number of times to repeat str
+     */
     public static String repeatString(String str, int n) {
         StringBuilder strBdr = new StringBuilder();
         for (int i = 0; i<n; i++) {
