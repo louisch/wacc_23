f17211e74166b1a22b61ceded84110e3c533d73b
Alan Han <alan.y.han@gmail.com>
2014-11-15 17:18:30 +0000

    Added more info in NullAST comment

--- src/wacc23/ast/expr/NullAST.java
+++ src/wacc23/ast/expr/NullAST.java
@@ -3,4 +3,5 @@ package wacc23.ast.expr;
 public class NullAST extends ExprAST {
     // this just represents null (and is only used in PAIR_LITER)
     // e.g. pair(int, int) myPair = null;
+    // calling getType() and check() should throw exceptions
 }
